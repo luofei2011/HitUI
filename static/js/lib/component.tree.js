c70db95937a87ac739243c67c8e467c3e9b5d429
Freemen <freemensmall@gmail.com>
2014-05-12 13:55:10 +0900

    add some树的操作

--- static/js/lib/component.tree.js
+++ static/js/lib/component.tree.js
@@ -5,11 +5,86 @@
  * */
 
 var treeDef = {
+	//the dataStructer definition of the tree
 	_treeNode: {},
 
+	/*
+	 *
+	 */
+	initTree: function(treeType){
+		this._treeNode.type = treeType;
+		$('.tree-area').append('<ul class='+treeType+' code=treeroot><li code=root level=0 class=tree-node ></li></ul>');
+	},
+
+	/*
+	 * 初始化时 添加树节点
+	 */
+	addTreeNode: function(type, name, code, father, level, index, leafFlag, taskFlag, task) {
+		//--get this tree according to the type
+		var theTree = $('.tree-area .' + type);
+
+		//--Father Node
+		var father_ul= theTree.find('ul[code=' + father + ']');
+		//TODO: 若无father，临时构建空的父节点。
+		//先默认会按顺序addTreeNode，除了root不考虑乱序
+		
+		//若father列表（ul）不存在，则在father项（li）下创造一个father列表
+		if ( !father_ul.length ) {
+			theTree.find('li[code=' + father + ']').append('<ul code=' + father + ' level=' + (level-1) + ' class=tree-list ></ul>');
+			var father_ul= theTree.find('ul[code=' + father + ']');
+		}
+
+		//--Children Node
+		var child_li = theTree.find('li[code=' + code + ']');
+		//若存在此code，则更新内容
+		if ( child_li.length ) {
+			child_li.prepend('<p>' + name + '</p>');
+		} else {
+			//在father列表（ul）底下增加子列表项
+			//TODO: 增加的时候还需考虑顺序
+			father_ul.append('<li code=' + code + ' level=' + level + ' leaf=' + leafFlag + ' class=tree-node ><p>'+name+'</p></li>');	
+		}
+	},
+
+	/*
+	 * _addSon is just for the initialization of the Tree
+	 * Dynamicaly adding function should be implemented through Database
+	 * 
+	 */
+	//_addSon: function( father, son ){}
+
+	/*
+	 *
+	 */
+	setOpenNode: function(type, nodes) {
+		//--get this tree according to the type
+		var theTree = $('.tree-area .' + type);
+
+		//set the open list
+		var selectedNode = theTree;
+		for (len = nodes.length, i = 0; i < len; i++) {
+			var node = theTree.find('li[code=' + nodes[i] + ']');
+			//set all it's parent as open node
+			node.parentsUntil(theTree, 'li').attr('opened', 'Y');
+			//if openNode is leaf then select it
+			if(node.attr('leaf') == 'Y') {
+				selectedNode = node;	//only the last one node could be choosen
+			} else {
+				node.attr('opened', 'Y');
+			}
+		}
+		//hide the hiden, show the shown
+		theTree.find('ul').hide();
+		theTree.find('li[opened="Y"]').children('ul').show();
+		//set selected
+		selectedNode.attr('select', 'Y');
+		selectedNode.css('background-color', '#8ab');
+		return selectedNode;
+	}
 
 };
 
+//add tree to hit.COMPONENT
 //如果已经定义COMPONENT，则在后面添加tree
 if ( hit.COMPONENT ){
 	hit.COMPONENT.tree = treeDef;
