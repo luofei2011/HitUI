7fa9ba6322fda5bb67c98bacef6c33f17590971d
luofei2011 <luofeihit2010@gmail.com>
2014-05-29 21:27:32 +0800

    update

---
+++ static/js/config/table_inv_bill_detail.js
@@ -0,0 +1,335 @@
+/*
+ *  数据表inv_bill_main配置文件
+ * */
+if (typeof hit === 'undefined')
+    hit = {};
+if (typeof hit.CONFIG === 'undefined')
+    hit.CONFIG = {};
+
+hit.CONFIG.table_inv_bill_main = {
+	"db": {
+		"name": "inv",
+		"t": "inv_bill_detail"
+	},
+	"url": "http://localhost/graduation-pro-2014/load/deal_data",
+	"rows": 2,
+	"isShowPaging": true,
+	"pageNum": 50,
+	"hasCheckBox": true,
+	"hasQuery": true,
+	"qParam": [{
+		"name": "",
+		"label": "",
+		"type": "",
+		"required": false
+	}],
+	"hasFunc": true,
+	"funcArr": ["add", "edit", "delete", "save"],
+	"widthMsg": [120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120, 120],
+	"headContent": [[{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	},
+	{
+		"label": "",
+		"isFixed": false,
+		"isOrdered": false,
+		"align": "center",
+		"multiply": false,
+		"colspan": 2
+	}]],
+	"bodyContent": {
+		"Bill_num": {
+			"isKey": true,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Serial_num": {
+			"isKey": true,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_code": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_name": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_type": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_unit": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_spec": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_texture": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_gb": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_batch": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_qty": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_price": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_sum": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "required",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_quality_degree": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"Item_IO_type": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		},
+		"file_path": {
+			"isKey": false,
+			"isShow": true,
+			"align": "left",
+			"canEdit": true,
+			"type": "text",
+			"valid": "",
+			"sureValue": "",
+			"minLen": "",
+			"maxLen": ""
+		}
+	}
+}
